bf3ec693f175c81035a59df39cdb9673d2806c04
Maximiliano Fierro <elmasse@gmail.com>
2014-11-28 21:38:23 -0300

    adding delegates example

--- src/my-element.js
+++ src/my-element.js
@@ -6,10 +6,25 @@ cement.mix({
     '@element'   : 'my-element',
     '@prototype' : HTMLElement,
 
+    '@delegates' : {
+        'h1': {
+            click: 'onMessageClick'
+        }
+    },
+
     createdCallback: function () {
         var me = this;
 
         me.innerHTML = "<h1>Hello</h1>";
+    },
+
+    onMessageClick: function () {
+        var h1 = this.querySelector('h1'),
+            color = "#"+((1<<24)*Math.random()|0).toString(16);
+
+        h1.style.backgroundColor = color;
+        console.log('message click');
     }
+
 
 });
