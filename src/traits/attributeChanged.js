396d0df6f03c4313c48a1a79d92187196278b27d
Maximiliano Fierro <elmasse@gmail.com>
2015-09-24 17:22:49 -0300

    npm run lint. added eslint configs

--- src/traits/attributeChanged.js
+++ src/traits/attributeChanged.js
@@ -2,7 +2,7 @@ import { requires } from 'traits-decorator';
 import {ATTR_CHANGED_MAP} from '../constants';
 
 const attributeChangedCallback = {
-    
+
     @requires('Symbol(ATTR_CHANGED_MAP):{Function}')
     attributeChangedCallback (attrName, oldVal, newVal) {
         const me = this,
