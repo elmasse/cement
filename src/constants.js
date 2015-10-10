3b4a589fcbc14621075470ec8ddd8b08a35a52b2
Maximiliano Fierro <elmasse@gmail.com>
2015-10-10 20:29:24 -0300

    tidying up

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,4 @@
 
 export const ATTR_CHANGED_MAP = Symbol.for('__ATTR_CHANGED__');
-export const LISTENERS = Symbol.for('__LISTENERS__');
-export const TEMPLATE = Symbol.for('__TEMPLATE__');
-
+export const LISTENERS        = Symbol.for('__LISTENERS__');
+export const TEMPLATE         = Symbol.for('__TEMPLATE__');
